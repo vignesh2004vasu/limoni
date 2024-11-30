1bcf1e03880f35c25f27c057a31a84990a2e8db8
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-11-30 09:41:53 +0530

    hello file commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello vignesh</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+
+  text: {
+    color: "#FFFFFF",
+    fontSize: 30,
+  },
+});
